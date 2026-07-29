fd152c9e9d0a59e87cca66f767b857e25f5dc3c3
0xiloveyou <278306338+0xiloveyou@users.noreply.github.com>
2026-07-29 11:21:05 +0600

    docs: updated project folder structure note/reference

--- current.file.structure.ts
+++ current.file.structure.ts
@@ -1,5 +1,30 @@
 /*
 
 
+app
+├── (public)
+│   ├── page.tsx                  // Home
+│
+├── (auth)
+
+│
+├── (dashboard)
+│   └── 
+│
+├── loading.tsx
+
+root
+│
+├── component
+│   ├── shared
+│   |    ├── Navbar
+│   |    ├── Footer
+│   |
+│   ├── Ui
+|   |    ├── button.tsx
+|   |    ├── card.tsx
+├──
+
+
 
 */
